0e428444bfc2ccc6153f396fe3bfb0adf198efe7
Soren Hansen <soren@linux2go.dk>
2013-11-21 16:48:50 +0100

    Remove debug logs from wsgi.py

--- gammelnok/wsgi.py
+++ gammelnok/wsgi.py
@@ -14,18 +14,6 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from opbeat.contrib.django.middleware.wsgi import Opbeat
 
-class MyCling(Cling):
-    def _should_handle(self, path):
-        """Checks if the path should be handled. Ignores the path if:
-
-        * the host is provided as part of the base_url
-        * the request's path isn't under the media path (or equal)
-        """
-        import glob
-        print glob.glob('*')
-        return path.startswith(self.base_url[2]) and not self.base_url[1]
-
-
 application = get_wsgi_application()
-application = MyCling(application)
+application = Cling(application)
 application = Opbeat(application)
