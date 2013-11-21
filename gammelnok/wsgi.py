41fec807e59456f695840ee78feb7a52a701ea17
Soren Hansen <soren@linux2go.dk>
2013-11-21 15:56:52 +0100

    More debugging

--- gammelnok/wsgi.py
+++ gammelnok/wsgi.py
@@ -13,6 +13,18 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gammelnok.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from opbeat.contrib.django.middleware.wsgi import Opbeat
+
+class MyCling(Cling):
+    def _should_handle(self, path):
+        """Checks if the path should be handled. Ignores the path if:
+
+        * the host is provided as part of the base_url
+        * the request's path isn't under the media path (or equal)
+        """
+        print path, self.base_url
+        return path.startswith(self.base_url[2]) and not self.base_url[1]
+
+
 application = get_wsgi_application()
-application = Cling(application)
+application = MyCling(application)
 application = Opbeat(application)
