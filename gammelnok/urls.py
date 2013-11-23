fbc39ec75226e63ca29ba167d5cca2817c47dcf1
Soren Hansen <soren@linux2go.dk>
2013-11-23 23:59:18 +0100

    Hook up / in urls.py

--- gammelnok/urls.py
+++ gammelnok/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^signup/edit/(?P<uuid>\S+)/$', 'event.views.signup_edit', name='signup_edit'),
     url(r'^signup/(?P<event_id>\S+)/$', 'event.views.signup', name='signup'),
     url(r'^signup/', 'event.views.signup_top', name='signup_top'),
+    url(r'^$', 'event.views.front', name='front'),
     url(r'^thanks/(?P<event_id>\S+)/$', 'event.views.thanks', name='thanks'),
     url(r'^admin/', include(admin.site.urls)),
 )
